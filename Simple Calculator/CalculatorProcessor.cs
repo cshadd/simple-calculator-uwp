b16cbd5f8e0f69de5e1cc3eab6bde81c1049809c
cshadd <christian.v.t.shadd@gmail.com>
2019-09-23 05:36:07 -0700

    Updated layout, added functionality, and dlls

---
+++ Simple Calculator/CalculatorProcessor.cs
@@ -0,0 +1,228 @@
+﻿using NCalc;
+using System.Collections.Generic;
+
+namespace Simple_Calculator
+{
+    public class CalculatorProcessor
+    {
+        private Problem currentProblem;
+        public List<Problem> history { get; private set; }
+
+        private bool Halt { get; set; }
+        private bool NoDelete { get; set; }
+
+        public CalculatorProcessor() : base()
+        {
+            this.history = new List<Problem>();
+            this.currentProblem = new Problem(0);
+            this.Halt = false;
+            this.NoDelete = false;
+            return;
+        }
+
+        public class Problem
+        {
+            private Expression solver;
+
+            public string Raw { get; set; }
+            private string Result { get; set; }
+
+            public Problem() : this("")
+            {
+                return;
+            }
+
+            public Problem(double raw) : this(raw + "")
+            {
+                return;
+            }
+
+            public Problem(string raw) : base()
+            {
+                this.Raw = raw;
+                this.Result = null;
+                this.solver = null;
+            }
+
+            public bool NegativeExists()
+            {
+                return this.Raw.Contains("n");
+            }
+
+            public void SetRawWithNumber(double number)
+            {
+                this.Raw = number + "";
+                if (number < 0)
+                {
+                    this.Raw = "n" + this.Raw;
+                }
+                return;
+            }
+
+            public string Solve()
+            {
+                this.solver = new Expression(this.Raw.Replace("n", "-1*"));
+                this.Result = this.solver.Evaluate() + "";
+                return this.Result;
+            }
+        }
+
+        public void AppendDecimal()
+        {
+            this.currentProblem.Raw += ".";
+            return;
+        }
+
+        public void AppendNumberToOperation(int number)
+        {
+            if (this.currentProblem.Raw.Equals("0"))
+            {
+                this.currentProblem.SetRawWithNumber(number);
+            }
+            else
+            {
+                this.currentProblem.Raw += number;
+            }
+            this.Halt = false;
+            return;
+        }
+
+        public void Add()
+        {
+            this.currentProblem.Raw += " + ";
+            this.Halt = true;
+            return;
+        }
+
+        public void C()
+        {
+            this.currentProblem.SetRawWithNumber(0);
+            return;
+        }
+
+        public void CE()
+        {
+            var components = this.currentProblem.Raw.Split();
+            var endOf = this.currentProblem.Raw.LastIndexOf(components[components.Length - 1]);
+            var canRemove = double.TryParse(this.currentProblem.Raw.Substring(endOf), out double result);
+            if (canRemove)
+            {
+                this.currentProblem.Raw = this.currentProblem.Raw.Substring(0, endOf);
+            }
+
+            if (components.Length == 1)
+            {
+                this.currentProblem.SetRawWithNumber(0);
+            }
+            return;
+        }
+
+        public void ClearHistory()
+        {
+            this.history.Clear();
+            return;
+        }
+
+        public void Delete()
+        {
+            if (!this.NoDelete)
+            {
+                if (this.currentProblem.NegativeExists() && this.currentProblem.Raw.Length > 2)
+                {
+                    this.currentProblem.Raw = this.currentProblem.Raw.Substring(0, this.currentProblem.Raw.Length - 1);
+                }
+                else if (this.currentProblem.NegativeExists() && this.currentProblem.Raw.Length == 2)
+                {
+                    this.CE();
+                }
+                else if (this.currentProblem.Raw.Length > 1)
+                {
+                    this.currentProblem.Raw = this.currentProblem.Raw.Substring(0, this.currentProblem.Raw.Length - 1);
+                }
+                else
+                {
+                    this.CE();
+                }
+            }
+            return;
+        }
+
+        public void Div()
+        {
+            this.currentProblem.Raw += " / ";
+            this.Halt = true;
+            return;
+        }
+
+        public void OneOverX()
+        {
+            this.currentProblem.Raw = "1/(" + this.currentProblem.Raw + ")";
+            this.Halt = true;
+            return;
+        }
+
+        public void Mul()
+        {
+            this.currentProblem.Raw += " x ";
+            this.Halt = true;
+            return;
+        }
+
+        public void Percent()
+        {
+            this.currentProblem.Raw = this.currentProblem.Raw + "%";
+            return;
+        }
+
+        public void ReverseOperationNumber()
+        {
+            if (!this.currentProblem.Raw.Equals("0"))
+            {
+                if (this.currentProblem.NegativeExists())
+                {
+                    this.currentProblem.Raw = this.currentProblem.Raw.Substring(1);
+                }
+                else
+                {
+                    this.currentProblem.Raw = "n" + this.currentProblem.Raw;
+                }
+            }
+            return;
+        }
+
+        public object Solve()
+        {
+            this.NoDelete = false;
+            this.history.Add(this.currentProblem);
+            var result = this.currentProblem.Solve();
+            this.currentProblem.Raw = result;
+            return result;
+        }
+
+        public void Sqr()
+        {
+            this.currentProblem.Raw = "(" + this.currentProblem.Raw + "*" + this.currentProblem.Raw + ")";
+            this.NoDelete = true;
+            return;
+        }
+
+        public void Sqrt()
+        {
+            this.currentProblem.Raw = "Sqrt(" + this.currentProblem.Raw + ")";
+            this.NoDelete = true;
+            return;
+        }
+
+        public void Sub()
+        {
+            this.currentProblem.Raw += " - ";
+            this.Halt = true;
+            return;
+        }
+
+        public override string ToString()
+        {
+            return this.currentProblem.Raw.Replace("n", "-");
+        }
+    }
+}
--- Simple Calculator/MainPage.xaml.cs
+++ Simple Calculator/MainPage.xaml.cs
@@ -1,35 +1,224 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+using System.Diagnostics;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
 namespace Simple_Calculator
 {
     /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// The calculator page.
     /// </summary>
     public sealed partial class MainPage : Page
     {
-        public MainPage()
+        private bool ShowingHistory { get; set; }
+        private CalculatorProcessor processor;
+
+        public MainPage() : base()
         {
             this.InitializeComponent();
+            this.ShowingHistory = false;
+            this.processor = new CalculatorProcessor();
+            var calculatorHistoryStack = this.FindName("CalculatorHistoryStack") as StackPanel;
+            var newPlaceHolder = new TextBlock();
+            newPlaceHolder.Text = "There's no history yet";
+            newPlaceHolder.FontSize = 20;
+            calculatorHistoryStack.Children.Add(newPlaceHolder);
+            return;
         }
 
-        private void Exit_Tapped(object sender, TappedRoutedEventArgs e)
+        /// <summary>
+        /// Invokes a calculator operation based on the sender's name. In this
+        /// case a button that is pressed will determine the action to take.
+        /// </summary>
+        /// <param name="sender">The sender object that called the method</param>
+        /// <param name="e">Details about the tapped route</param>
+        private void Operation(object sender, TappedRoutedEventArgs e)
         {
-            Application.Current.Exit();
+            var clickedElementName = ((FrameworkElement)sender).Name;
+            Debug.WriteLine("Noga did an operation with " + clickedElementName);
+
+            var calculatorHistoryStack = this.FindName("CalculatorHistoryStack") as StackPanel;
+            var operatorTrashHistory = this.FindName("Operator_TrashHistory") as Button;
+            
+            if (clickedElementName == "Operator_Exit")
+            {
+                Application.Current.Exit();
+            }
+            else if (clickedElementName == "Operator_History")
+            {
+                var calculatorButtons = this.FindName("CalculatorButtons") as Grid;
+                var calculatorHistory = this.FindName("CalculatorHistory") as Grid;
+
+                if (this.ShowingHistory)
+                {
+                    calculatorButtons.Visibility = Visibility.Visible;
+                    calculatorHistory.Visibility = Visibility.Collapsed;
+                }
+                else
+                {
+                    calculatorButtons.Visibility = Visibility.Collapsed;
+                    calculatorHistory.Visibility = Visibility.Visible;
+                }
+                this.ShowingHistory = !this.ShowingHistory;
+            }
+            else if (clickedElementName == "Operator_TrashHistory")
+            {
+                this.processor.ClearHistory();
+                calculatorHistoryStack.Children.Clear();
+                var newPlaceHolder = new TextBlock();
+                newPlaceHolder.Text = "There's no history yet";
+                newPlaceHolder.FontSize = 20;
+                calculatorHistoryStack.Children.Add(newPlaceHolder);
+                operatorTrashHistory.Visibility = Visibility.Collapsed;
+            }
+            else if (clickedElementName == "Operator_Percent")
+            {
+                this.processor.Percent();
+            }
+            else if (clickedElementName == "Operator_Sqrt")
+            {
+                this.processor.Sqrt();
+            }
+            else if (clickedElementName == "Operator_Sqr")
+            {
+                this.processor.Sqr();
+            }
+            else if (clickedElementName == "Operator_1Overx")
+            {
+                this.processor.OneOverX();
+            }
+            else if (clickedElementName == "Operator_CE")
+            {
+                this.processor.CE();
+            }
+            else if (clickedElementName == "Operator_C")
+            {
+                this.processor.C();
+            }
+            else if (clickedElementName == "Operator_Del")
+            {
+                this.processor.Delete();
+            }
+            else if (clickedElementName == "Operator_Div")
+            {
+                this.processor.Div();
+            }
+            else if (clickedElementName == "Operator_Mul")
+            {
+                this.processor.Mul();
+            }
+            else if (clickedElementName == "Operator_Sub")
+            {
+                this.processor.Sub();
+            }
+            else if (clickedElementName == "Operator_Add")
+            {
+                this.processor.Add();
+            }
+            else if (clickedElementName == "Operator_Dot")
+            {
+                this.processor.AppendDecimal();
+            }
+            else if (clickedElementName == "Operator_Reverse")
+            {
+                this.processor.ReverseOperationNumber();
+            }
+            else if (clickedElementName == "Operator_0")
+            {
+                this.processor.AppendNumberToOperation(0);
+            }
+            else if (clickedElementName == "Operator_1")
+            {
+                this.processor.AppendNumberToOperation(1);
+            }
+            else if (clickedElementName == "Operator_2")
+            {
+                this.processor.AppendNumberToOperation(2);
+            }
+            else if (clickedElementName == "Operator_3")
+            {
+                this.processor.AppendNumberToOperation(3);
+            }
+            else if (clickedElementName == "Operator_4")
+            {
+                this.processor.AppendNumberToOperation(4);
+            }
+            else if (clickedElementName == "Operator_5")
+            {
+                this.processor.AppendNumberToOperation(5);
+            }
+            else if (clickedElementName == "Operator_6")
+            {
+                this.processor.AppendNumberToOperation(6);
+            }
+            else if (clickedElementName == "Operator_7")
+            {
+                this.processor.AppendNumberToOperation(7);
+            }
+            else if (clickedElementName == "Operator_8")
+            {
+                this.processor.AppendNumberToOperation(8);
+            }
+            else if (clickedElementName == "Operator_9")
+            {
+                this.processor.AppendNumberToOperation(9);
+            }
+
+            // Refresh Result
+            var result = this.FindName("CalculatorResult") as TextBlock;
+            result.Text = this.processor.ToString();
+
+            if (clickedElementName == "Operator_Equal")
+            {
+                try
+                {
+                    result.Text = "" + this.processor.Solve();
+                    if (this.processor.history.Count == 1)
+                    {
+                        calculatorHistoryStack.Children.Clear();
+                    }
+
+                    var newPlaceHolder = new TextBlock();
+                    newPlaceHolder.TextAlignment = TextAlignment.Right;
+                    newPlaceHolder.Margin = new Thickness(0, 0, 10, 0);
+                    newPlaceHolder.FontSize = 20;
+                    newPlaceHolder.Text = (this.processor.history[this.processor.history.Count - 1].Raw + " = ").Replace("n", "-");
+                    calculatorHistoryStack.Children.Add(newPlaceHolder);
+
+                    newPlaceHolder = new TextBlock();
+                    newPlaceHolder.TextAlignment = TextAlignment.Right;
+                    newPlaceHolder.Margin = new Thickness(0, 0, 10, 0);
+                    newPlaceHolder.FontSize = 36;
+                    newPlaceHolder.Text = result.Text;
+                    calculatorHistoryStack.Children.Add(newPlaceHolder);
+                }
+                catch (NCalc.EvaluationException ex)
+                {
+                    result.Text = "Malformed expression!";
+                    Debug.WriteLine(ex);
+                }
+                catch (Exception ex)
+                {
+                    result.Text = "Unknown error!";
+                    Debug.WriteLine(ex);
+                }
+                finally { }
+            }
+
+            // Recalculate History
+            if (this.processor.history.Count > 0)
+            {
+                operatorTrashHistory.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                operatorTrashHistory.Visibility = Visibility.Collapsed;
+            }
+
+            return;
         }
     }
 }
