635b44b3ee16cf25789ab2028175903dab758f9a
cshadd <christian.v.t.shadd@gmail.com>
2019-09-23 13:42:23 -0700

    Finished functionality and documentation.

--- Simple Calculator/CalculatorProcessor.cs
+++ Simple Calculator/CalculatorProcessor.cs
@@ -1,54 +1,158 @@
-﻿using NCalc;
+﻿// ***********************************************************************
+// Assembly         : Simple Calculator
+// Author           : Christian
+// Created          : 09-23-2019
+//
+// Last Modified By : Christian
+// Last Modified On : 09-23-2019
+// ***********************************************************************
+// <copyright file="CalculatorProcessor.cs" company="">
+//     Copyright © 2019 Christian Shadd
+// </copyright>
+// <summary></summary>
+// ***********************************************************************
+using NCalc;
 using System.Collections.Generic;
 
 namespace Simple_Calculator
 {
+    /// <summary>
+    /// A processor for a calculator using basic integers and other symbols
+    /// to make strings that are solved in a <see cref="Problem" /> object.
+    /// </summary>
     public class CalculatorProcessor
     {
-        private Problem currentProblem;
-        public List<Problem> history { get; private set; }
+        /// <summary>
+        /// The <see cref="Problem" /> object currently in use.
+        /// </summary>
+        private Problem problem;
 
-        private bool Halt { get; set; }
+        /// <summary>
+        /// A <see cref="Stack{T}" /> of <see cref="Problem" /> objects in
+        /// history.
+        /// </summary>
+        /// <value>The history.</value>
+        public Stack<Problem> history { get; private set; }
+
+        /// <summary>
+        /// Prevents deletion from the <see cref="Problem" />'s
+        /// raw value until the Problem has been solved.
+        /// </summary>
+        /// <value><c>true</c> if [no delete]; otherwise, <c>false</c>.</value>
+        /// <remarks>Sometimes there is an operation that changes the syntax of the
+        /// Problem's Raw value. In the case where this holds true, it
+        /// would be unwise to delete the new syntax as this can cause a
+        /// <see cref="EvaluationException" /> in the program.</remarks>
         private bool NoDelete { get; set; }
 
+        /// <summary>
+        /// A solver for solving <see cref="Problem" />s.
+        /// </summary>
+        /// <seealso cref="Expression" />
+        private Expression solver;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CalculatorProcessor" />
+        /// class.
+        /// </summary>
         public CalculatorProcessor() : base()
         {
-            this.history = new List<Problem>();
-            this.currentProblem = new Problem(0);
-            this.Halt = false;
+            this.history = new Stack<Problem>();
+            this.problem = new Problem(0);
             this.NoDelete = false;
+            this.solver = null;
             return;
         }
 
+        #region Problem
+        /// <summary>
+        /// A problem container that has a raw value and a result value.
+        /// to solve.
+        /// </summary>
         public class Problem
         {
-            private Expression solver;
-
+            /// <summary>
+            /// Gets or sets the raw value of the problem.
+            /// </summary>
+            /// <value>The raw value to set.</value>
+            /// <remarks>This can be an expression of any
+            /// type.</remarks>
             public string Raw { get; set; }
-            private string Result { get; set; }
 
+            /// <summary>
+            /// Gets or sets the result value of the
+            /// problem.
+            /// </summary>
+            /// <value>The result value to set.</value>
+            public string Result { get; set; }
+
+            /// <summary>
+            /// Initializes a new instance of the <see cref="Problem" />
+            /// class.
+            /// </summary>
             public Problem() : this("")
             {
                 return;
             }
 
+            /// <summary>
+            /// Initializes a new instance of the <see cref="Problem" />
+            /// class.
+            /// </summary>
+            /// <param name="raw">The raw value to set.</param>
             public Problem(double raw) : this(raw + "")
             {
                 return;
             }
 
+            /// <summary>
+            /// Initializes a new instance of the <see cref="Problem" />
+            /// class.
+            /// </summary>
+            /// <param name="raw">The raw value to set.</param>
             public Problem(string raw) : base()
             {
                 this.Raw = raw;
                 this.Result = null;
-                this.solver = null;
             }
 
+            /// <summary>
+            /// Checks for negatives in the <see cref="Raw" /> value.
+            /// </summary>
+            /// <returns><c>true</c> if negative exists, <c>false</c>
+            /// otherwise.</returns>
             public bool NegativeExists()
             {
                 return this.Raw.Contains("n");
             }
 
+            /// <summary>
+            /// Returns a "proper" expression with any <c>n</c>
+            /// replaced with a <c>-</c> for negative
+            /// numbers.
+            /// </summary>
+            /// <returns>A <see cref="string" /> value.</returns>
+            public string ProperExpression()
+            {
+                return this.Raw.Replace("n", "-");
+            }
+
+            /// <summary>
+            /// Returns a "proper" formatted problem with any <c>n</c>
+            /// replaced with a <c>-1*</c> for negative
+            /// numbers.
+            /// </summary>
+            /// <returns>A <see cref="string" /> value.</returns>
+            public string ProperFormat()
+            {
+                return this.Raw.Replace("n", "-1*");
+            }
+
+            /// <summary>
+            /// Sets the <see cref="Raw" /> value with number.
+            /// </summary>
+            /// <param name="number">The number to set as the raw
+            /// value.</param>
             public void SetRawWithNumber(double number)
             {
                 this.Raw = number + "";
@@ -58,171 +162,264 @@ namespace Simple_Calculator
                 }
                 return;
             }
-
-            public string Solve()
-            {
-                this.solver = new Expression(this.Raw.Replace("n", "-1*"));
-                this.Result = this.solver.Evaluate() + "";
-                return this.Result;
-            }
         }
+        #endregion
 
+        /// <summary>
+        /// Appends a decimal to the <see cref="problem" />.
+        /// </summary>
         public void AppendDecimal()
         {
-            this.currentProblem.Raw += ".";
+            this.problem.Raw += ".";
             return;
         }
 
-        public void AppendNumberToOperation(int number)
+        /// <summary>
+        /// Appends a number to the <see cref="problem" />.
+        /// </summary>
+        /// <param name="number">The number to append.</param>
+        public void AppendNumber(int number)
         {
-            if (this.currentProblem.Raw.Equals("0"))
+            // If the raw value is just "0"...
+            if (this.problem.Raw.Equals("0"))
             {
-                this.currentProblem.SetRawWithNumber(number);
+                // Set problem with number.
+                this.problem.SetRawWithNumber(number);
             }
             else
             {
-                this.currentProblem.Raw += number;
+                // Append number to raw value.
+                this.problem.Raw += number;
             }
-            this.Halt = false;
             return;
         }
 
+        /// <summary>
+        /// Commits an addition statement to the <see cref="problem" />.
+        /// </summary>
         public void Add()
         {
-            this.currentProblem.Raw += " + ";
-            this.Halt = true;
+            this.problem.Raw += " + ";
             return;
         }
 
+        /// <summary>
+        /// Clears the <see cref="problem" /> by setting it to 0.
+        /// </summary>
         public void C()
         {
-            this.currentProblem.SetRawWithNumber(0);
+            this.problem.SetRawWithNumber(0);
             return;
         }
 
+        /// <summary>
+        /// Clears last number entry of the <see cref="problem" />.
+        /// </summary>
         public void CE()
         {
-            var components = this.currentProblem.Raw.Split();
-            var endOf = this.currentProblem.Raw.LastIndexOf(components[components.Length - 1]);
-            var canRemove = double.TryParse(this.currentProblem.Raw.Substring(endOf), out double result);
+            // Split the problem by tokens.
+            var components = this.problem.Raw.Split();
+            // Get last token.
+            var lastToken = components[components.Length - 1];
+            // Check to see if last token is a number.
+            var canRemove = double.TryParse(lastToken, out double result);
+            // If last token is a number...
             if (canRemove)
             {
-                this.currentProblem.Raw = this.currentProblem.Raw.Substring(0, endOf);
+                // Get last index of last token.
+                var endOf = this.problem.Raw.LastIndexOf(lastToken);
+                // Remove last token via the last index.
+                this.problem.Raw = this.problem.Raw.Substring(0, endOf);
             }
 
+            // If there is only one token...
             if (components.Length == 1)
             {
-                this.currentProblem.SetRawWithNumber(0);
+                // Set problem back to 0.
+                this.problem.SetRawWithNumber(0);
             }
             return;
         }
 
+        /// <summary>
+        /// Clears the <see cref="history" /> of <see cref="Problem" />s.
+        /// </summary>
         public void ClearHistory()
         {
             this.history.Clear();
             return;
         }
 
+        /// <summary>
+        /// Deletes last value of <see cref="problem" />'s raw value.
+        /// </summary>
+        /// <remarks>This handles especially well for negatives.</remarks>
         public void Delete()
         {
+            // If we can delete...
             if (!this.NoDelete)
             {
-                if (this.currentProblem.NegativeExists() && this.currentProblem.Raw.Length > 2)
+                // If there is a negative and the problem has more than two
+                // values, such that the first one is a negative with
+                // values after it...
+                if (this.problem.NegativeExists()
+                    && this.problem.Raw.Length > 2)
                 {
-                    this.currentProblem.Raw = this.currentProblem.Raw.Substring(0, this.currentProblem.Raw.Length - 1);
+                    // Remove last value.
+                    this.problem.Raw = this.problem.Raw.Substring(0,
+                        this.problem.Raw.Length - 1);
                 }
-                else if (this.currentProblem.NegativeExists() && this.currentProblem.Raw.Length == 2)
+                // If there is a negative and the problem has two values,
+                // such that the first one is a negative with values
+                // after it...
+                else if (this.problem.NegativeExists()
+                    && this.problem.Raw.Length == 2)
                 {
+                    // Clear last entry.
                     this.CE();
                 }
-                else if (this.currentProblem.Raw.Length > 1)
+                // If the problem has more than one value...
+                else if (this.problem.Raw.Length > 1)
                 {
-                    this.currentProblem.Raw = this.currentProblem.Raw.Substring(0, this.currentProblem.Raw.Length - 1);
+                    // Remove last value.
+                    this.problem.Raw = this.problem.Raw.Substring(0,
+                        this.problem.Raw.Length - 1);
                 }
                 else
                 {
+                    // Clear last entry.
                     this.CE();
                 }
             }
             return;
         }
 
+        /// <summary>
+        /// Commits an division statement to the <see cref="problem" />.
+        /// </summary>
         public void Div()
         {
-            this.currentProblem.Raw += " / ";
-            this.Halt = true;
+            this.problem.Raw += " / ";
             return;
         }
 
+        /// <summary>
+        /// Commits an 1/x statement to the <see cref="problem" />.
+        /// </summary>
         public void OneOverX()
         {
-            this.currentProblem.Raw = "1/(" + this.currentProblem.Raw + ")";
-            this.Halt = true;
+            this.problem.Raw = "1/(" + this.problem.Raw + ")";
+            this.NoDelete = true;
             return;
         }
 
+        /// <summary>
+        /// Commits an multiplication statement to the <see cref="problem" />.
+        /// </summary>
         public void Mul()
         {
-            this.currentProblem.Raw += " x ";
-            this.Halt = true;
+            this.problem.Raw += " x ";
             return;
         }
 
+        /// <summary>
+        /// Commits an % statement to the <see cref="problem" />.
+        /// </summary>
         public void Percent()
         {
-            this.currentProblem.Raw = this.currentProblem.Raw + "%";
+            this.problem.Raw = this.problem.Raw + "%";
             return;
         }
 
+        /// <summary>
+        /// Reverses <see cref="problem" /> from negative to positive.
+        /// </summary>
         public void ReverseOperationNumber()
         {
-            if (!this.currentProblem.Raw.Equals("0"))
+            // If the problem is "0"...
+            if (!this.problem.Raw.Equals("0"))
             {
-                if (this.currentProblem.NegativeExists())
+                // If the problem has a negative...
+                if (this.problem.NegativeExists())
                 {
-                    this.currentProblem.Raw = this.currentProblem.Raw.Substring(1);
+                    // Remove the first value, in this case a negative.
+                    this.problem.Raw = this.problem.Raw.Substring(1);
                 }
                 else
                 {
-                    this.currentProblem.Raw = "n" + this.currentProblem.Raw;
+                    // Prepend a negative value.
+                    this.problem.Raw = "n" + this.problem.Raw;
                 }
             }
             return;
         }
 
-        public object Solve()
+        /// <summary>
+        /// Solves the <see cref="problem" /> via evaluation.
+        /// </summary>
+        /// <returns><see cref="string" /> of the result.</returns>
+        /// <exception cref="EvaluationException">Problem cannot
+        /// be solved due to malformed tokens.</exception>
+        public string Solve()
         {
+            // Make the problem into a proper expression.
+            var proper = this.problem.ProperExpression();
+            // Solve the proper expression. 
+            this.solver = new Expression(proper);
+            // Turn result from solver into a string.
+            var result = this.solver.Evaluate() + "";
+            // Set the result of the solved problem.
+            this.problem.Result = result;
+            // Put solved problem into history.
+            this.history.Push(this.problem);
+            // Make a new problem with the result.
+            this.problem = new Problem(result);
+            // Characters can be deleted now.
             this.NoDelete = false;
-            this.history.Add(this.currentProblem);
-            var result = this.currentProblem.Solve();
-            this.currentProblem.Raw = result;
+            // Return our result.
             return result;
         }
 
+        /// <summary>
+        /// Commits an x^2 statement to the <see cref="problem" />.
+        /// </summary>
         public void Sqr()
         {
-            this.currentProblem.Raw = "(" + this.currentProblem.Raw + "*" + this.currentProblem.Raw + ")";
+            this.problem.Raw = "(" + this.problem.Raw + "*" + this.problem.Raw + ")";
             this.NoDelete = true;
             return;
         }
 
+        /// <summary>
+        /// Commits an sqrt(x) statement to the <see cref="problem" />.
+        /// </summary>
         public void Sqrt()
         {
-            this.currentProblem.Raw = "Sqrt(" + this.currentProblem.Raw + ")";
+            this.problem.Raw = "Sqrt(" + this.problem.Raw + ")";
             this.NoDelete = true;
             return;
         }
 
+        /// <summary>
+        /// Commits an subtraction statement to the <see cref="problem" />.
+        /// </summary>
         public void Sub()
         {
-            this.currentProblem.Raw += " - ";
-            this.Halt = true;
+            this.problem.Raw += " - ";
             return;
         }
 
+        /// <summary>
+        /// Returns a <see cref="string" /> that represents this instance. The
+        /// result is a proper format of the <see cref="problem" />.
+        /// </summary>
+        /// <returns>A <see cref="string" /> that represents this
+        /// instance.</returns>
+        /// <seealso cref="object" />
         public override string ToString()
         {
-            return this.currentProblem.Raw.Replace("n", "-");
+            base.ToString();
+            return this.problem.ProperFormat();
         }
     }
 }
--- Simple Calculator/MainPage.xaml.cs
+++ Simple Calculator/MainPage.xaml.cs
@@ -1,4 +1,17 @@
-﻿using System;
+﻿// ***********************************************************************
+// Assembly         : Simple Calculator
+// Author           : Christian
+// Created          : 09-23-2019
+//
+// Last Modified By : Christian
+// Last Modified On : 09-23-2019
+// ***********************************************************************
+// <copyright file="MainPage.xaml.cs" company="">
+//     Copyright © 2019 Christian Shadd
+// </copyright>
+// <summary></summary>
+// ***********************************************************************
+using System;
 using System.Diagnostics;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
@@ -9,19 +22,42 @@ using Windows.UI.Xaml.Input;
 namespace Simple_Calculator
 {
     /// <summary>
-    /// The calculator page.
+    /// The main page.
     /// </summary>
-    public sealed partial class MainPage : Page
+    sealed partial class MainPage : Page
     {
+        /// <summary>
+        /// Gets or sets a value indicating whether history is shown.
+        /// </summary>
+        /// <value><c>true</c> if history is shown; otherwise,
+        /// <c>false</c>.</value>
         private bool ShowingHistory { get; set; }
+        /// <summary>
+        /// The processor that is used to calculate our expressions.
+        /// </summary>
+        /// <seealso cref="CalculatorProcessor"/>
         private CalculatorProcessor processor;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MainPage" /> class.
+        /// </summary>
         public MainPage() : base()
         {
             this.InitializeComponent();
             this.ShowingHistory = false;
             this.processor = new CalculatorProcessor();
+            this.CreateHistoryPlaceHolder();
+            Debug.WriteLine("I love calculators!", "NOGA");
+            return;
+        }
+
+        /// <summary>
+        /// Creates the history place holder.
+        /// </summary>
+        private void CreateHistoryPlaceHolder()
+        {
             var calculatorHistoryStack = this.FindName("CalculatorHistoryStack") as StackPanel;
+            calculatorHistoryStack.Children.Clear();
             var newPlaceHolder = new TextBlock();
             newPlaceHolder.Text = "There's no history yet";
             newPlaceHolder.FontSize = 20;
@@ -31,17 +67,17 @@ namespace Simple_Calculator
 
         /// <summary>
         /// Invokes a calculator operation based on the sender's name. In this
-        /// case a button that is pressed will determine the action to take.
+        /// case a button that is tapped will determine the action to take.
         /// </summary>
-        /// <param name="sender">The sender object that called the method</param>
-        /// <param name="e">Details about the tapped route</param>
+        /// <param name="sender">The sender object that called the method.</param>
+        /// <param name="e">Details about the tapped route.</param>
         private void Operation(object sender, TappedRoutedEventArgs e)
         {
             var clickedElementName = ((FrameworkElement)sender).Name;
-            Debug.WriteLine("Noga did an operation with " + clickedElementName);
+            Debug.WriteLine("Operation " + clickedElementName + " executed!", "NOGA");
 
             var calculatorHistoryStack = this.FindName("CalculatorHistoryStack") as StackPanel;
-            var operatorTrashHistory = this.FindName("Operator_TrashHistory") as Button;
+
             
             if (clickedElementName == "Operator_Exit")
             {
@@ -67,12 +103,7 @@ namespace Simple_Calculator
             else if (clickedElementName == "Operator_TrashHistory")
             {
                 this.processor.ClearHistory();
-                calculatorHistoryStack.Children.Clear();
-                var newPlaceHolder = new TextBlock();
-                newPlaceHolder.Text = "There's no history yet";
-                newPlaceHolder.FontSize = 20;
-                calculatorHistoryStack.Children.Add(newPlaceHolder);
-                operatorTrashHistory.Visibility = Visibility.Collapsed;
+                this.CreateHistoryPlaceHolder();
             }
             else if (clickedElementName == "Operator_Percent")
             {
@@ -128,87 +159,101 @@ namespace Simple_Calculator
             }
             else if (clickedElementName == "Operator_0")
             {
-                this.processor.AppendNumberToOperation(0);
+                this.processor.AppendNumber(0);
             }
             else if (clickedElementName == "Operator_1")
             {
-                this.processor.AppendNumberToOperation(1);
+                this.processor.AppendNumber(1);
             }
             else if (clickedElementName == "Operator_2")
             {
-                this.processor.AppendNumberToOperation(2);
+                this.processor.AppendNumber(2);
             }
             else if (clickedElementName == "Operator_3")
             {
-                this.processor.AppendNumberToOperation(3);
+                this.processor.AppendNumber(3);
             }
             else if (clickedElementName == "Operator_4")
             {
-                this.processor.AppendNumberToOperation(4);
+                this.processor.AppendNumber(4);
             }
             else if (clickedElementName == "Operator_5")
             {
-                this.processor.AppendNumberToOperation(5);
+                this.processor.AppendNumber(5);
             }
             else if (clickedElementName == "Operator_6")
             {
-                this.processor.AppendNumberToOperation(6);
+                this.processor.AppendNumber(6);
             }
             else if (clickedElementName == "Operator_7")
             {
-                this.processor.AppendNumberToOperation(7);
+                this.processor.AppendNumber(7);
             }
             else if (clickedElementName == "Operator_8")
             {
-                this.processor.AppendNumberToOperation(8);
+                this.processor.AppendNumber(8);
             }
             else if (clickedElementName == "Operator_9")
             {
-                this.processor.AppendNumberToOperation(9);
+                this.processor.AppendNumber(9);
             }
 
-            // Refresh Result
-            var result = this.FindName("CalculatorResult") as TextBlock;
-            result.Text = this.processor.ToString();
+            // Refresh Result after each button tap.
+            var calculatorResult = this.FindName("CalculatorResult") as TextBlock;
+            calculatorResult.Text = this.processor.ToString();
 
             if (clickedElementName == "Operator_Equal")
             {
                 try
                 {
-                    result.Text = "" + this.processor.Solve();
+                    var result = this.processor.Solve();
+                    calculatorResult.Text = result;
+
+                    Debug.WriteLine("Result is " + result + "!", "NOGA");
+
+                    // If history only has one value, which is most likely the
+                    // no history placeholder...
                     if (this.processor.history.Count == 1)
                     {
+                        // Clear history.
                         calculatorHistoryStack.Children.Clear();
                     }
 
+                    // Top entry of the history stack.
+                    var top = this.processor.history.Peek();
+
+                    // Add solved problem to history view.
                     var newPlaceHolder = new TextBlock();
                     newPlaceHolder.TextAlignment = TextAlignment.Right;
                     newPlaceHolder.Margin = new Thickness(0, 0, 10, 0);
                     newPlaceHolder.FontSize = 20;
-                    newPlaceHolder.Text = (this.processor.history[this.processor.history.Count - 1].Raw + " = ").Replace("n", "-");
+                    newPlaceHolder.Text = (top.ProperExpression() + " = ");
                     calculatorHistoryStack.Children.Add(newPlaceHolder);
 
                     newPlaceHolder = new TextBlock();
                     newPlaceHolder.TextAlignment = TextAlignment.Right;
                     newPlaceHolder.Margin = new Thickness(0, 0, 10, 0);
                     newPlaceHolder.FontSize = 36;
-                    newPlaceHolder.Text = result.Text;
+                    newPlaceHolder.Text = result;
                     calculatorHistoryStack.Children.Add(newPlaceHolder);
                 }
                 catch (NCalc.EvaluationException ex)
                 {
-                    result.Text = "Malformed expression!";
-                    Debug.WriteLine(ex);
+                    calculatorResult.Text = "Malformed expression!";
+                    Debug.WriteLine(ex, "ERROR");
                 }
                 catch (Exception ex)
                 {
-                    result.Text = "Unknown error!";
-                    Debug.WriteLine(ex);
+                    calculatorResult.Text = "Unknown error!";
+                    Debug.WriteLine(ex, "ERROR");
                 }
                 finally { }
             }
 
-            // Recalculate History
+            var operatorTrashHistory = this.FindName("Operator_TrashHistory") as Button;
+
+            // Recalculate the need to show the history trashcan after each
+            // button tap.
             if (this.processor.history.Count > 0)
             {
                 operatorTrashHistory.Visibility = Visibility.Visible;
